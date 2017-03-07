69b21fcee3db1a5ab43d6695e5531bc5a8ed95b6
lubinhui <1015750316@qq.com>
2017-03-07 16:39:12 +0800

    project

---
+++ app/src/main/java/com/example/a002387/myproject/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a002387.myproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
